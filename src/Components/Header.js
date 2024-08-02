0e541555e86d6b39019a0ae49f05901c8aa752e7
Akashajay-dot <akashajay339@gmail.com>
2024-08-02 10:50:58 +0530

    test

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import "../Styles/Header.css"
 import Logo from "../Assets/logo.png"
+import { Link } from 'react-router-dom'
 function Header() {
   return (
     <div className='Header'>
       <div className="logoSct">
+        <Link to="/">
         <img className ="logo" src={Logo} alt="" />
+        </Link>
       </div>
     </div>
   )
